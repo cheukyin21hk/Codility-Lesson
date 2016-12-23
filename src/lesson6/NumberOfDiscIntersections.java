63c93dc23d3fbe5ededcb2ca7736d7124e71a659
cheukyin21hk <Cheukyin21hk@gmail.com>
2016-12-23 17:40:45 +0800

    Lesson 6 : finally get understand the solution for NumberOfDiscIntersections

--- src/lesson6/NumberOfDiscIntersections.java
+++ src/lesson6/NumberOfDiscIntersections.java
@@ -4,54 +4,75 @@ package lesson6;
  * @author lohris
  *         Time Complexity = NLog(N)
  *         Space Complexity = N
+ *         provided Basic test case :
+ *          A[0] = 1 (-1, 0, 1)
+ *          A[1] = 5 (-4, 1, 6)
+ *          A[2] = 2 ( 0, 2, 4)
+ *          A[3] = 1 ( 2, 3, 4)
+ *          A[4] = 4 ( 0, 4, 8)
+ *          A[5] = 0 ( 5, 5, 5)
+ *          
+ *          Solution is from
+ *          http://stackoverflow.com/questions/4801242/algorithm-to-calculate-number-of-intersecting-discs
  */
 public class NumberOfDiscIntersections {
-    class Solution {
-	final int MAX_OF_INTERSECT = 10000000;
 
-	public int solution(int[] A) {
-	    int result = 0;
-	    result = handleAllRangePart(A);
-	    if (result == -1) {
-		return result;
-	    }
-	    return result;
-
+	public static void main(String[] args) {
+		int[] testData = { 1, 2147483647, 0 };
+		NumberOfDiscIntersections a = new NumberOfDiscIntersections();
+		a.runTest(testData);
 	}
 
-	private int handleAllRangePart(int[] A) {
-	    int numberOfWholeRange = 0;
-	    for (int i = 0; i < A.length; i++) {
-		if (A[i] >= A.length) {
-		    numberOfWholeRange++;
+	public void runTest(int[] testData) {
+		Solution proposedSolution = new Solution();
+		System.out.println(proposedSolution.solution(testData));
+	}
+
+	class Solution {
+
+		private final int MAX_RESULT_VALUE = 10000000;
+
+		public int solution(int[] A) {
+			int result = 0;
+			int[] discStartPoint = new int[A.length];
+			int[] discEndPoint = new int[A.length];
+
+			for (int i = 0, t = A.length - 1; i < A.length; i++) {
+				int s = calculateTheStartPoint(i, A[i]);
+				int e = calculateTheEndPoint(i, A[i], t);
+				discStartPoint[s]++;
+				discEndPoint[e]++;
+			}
+
+			int activeDisc = 0;
+			for (int i = 0; i < A.length; i++) {
+				if (discStartPoint[i] > 0) {
+					//Intersect of active disc and new start disc
+					result += activeDisc * discStartPoint[i];
+					//Intersect of new start disc.
+					result += discStartPoint[i] * (discStartPoint[i] - 1) / 2;
+					if (MAX_RESULT_VALUE < result) {
+						return -1;
+					}
+					activeDisc += discStartPoint[i];
+				}
+				activeDisc -= discEndPoint[i];
+			}
+			return result;
 		}
-	    }
 
-	    if (numberOfWholeRange > MAX_OF_INTERSECT) {
-		return -1;
-	    } else {
-		long result = (combination(numberOfWholeRange, 2) + (A.length - numberOfWholeRange)
-			* numberOfWholeRange);
-		if (result > MAX_OF_INTERSECT) {
-		    return -1;
-		} else {
-		    return (int) result;
+		private int calculateTheEndPoint(long i, long radius, int rightMostPoint) {
+			if (i + radius > rightMostPoint)
+				return rightMostPoint;
+			else
+				return (int) (i + radius);
 		}
-	    }
-	}
 
-	private long factorial(int n) {
-	    long result = 1;
-	    for (int i = 1; i <= n; i++) {
-		result *= i;
-	    }
-	    return result;
+		private int calculateTheStartPoint(long i, long radius) {
+			if (i > radius)
+				return (int) (i - radius);
+			else
+				return 0;
+		}
 	}
-
-	private long combination(int total, int pick) {
-	    long result = factorial(total)
-		    / (factorial(pick) * factorial(total - pick));
-	    return result;
-	}
-    }
 }
