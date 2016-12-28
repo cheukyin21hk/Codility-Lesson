ef645c61d2170a62bb6bcc6fc4ce67b7ec84e355
cheukyin21hk <Cheukyin21hk@gmail.com>
2016-12-28 11:28:10 +0800

    Lesson 7 : nesting solution.

---
+++ src/lesson7/Nesting.java
@@ -0,0 +1,62 @@
+package lesson7;
+
+import java.util.Stack;
+
+/**
+ * @author lohris
+ * Target BIG-O in O(N)
+ * Point to notice :
+ */
+public class Nesting {
+	public static void main(String args[]) {
+		new Nesting().runTest();
+	}
+
+	public void runTest() {
+		Solution s = new Solution();
+		System.out.println(s.solution("()()()(((()"));
+		System.out.println(s.solution(""));
+		System.out.println(s.solution("()()()"));
+		System.out.println(s.solution("(()()())"));
+		System.out.println(s.solution(")))))"));
+	}
+
+	class Solution {
+		public int solution(String S) {
+			if (isNested(S)) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+
+		private boolean isNested(String s) {
+			if (s == null || s.isEmpty()) {
+				return true;
+			}
+			Stack<Character> storedChar = new Stack<>();
+			Character tempChar = null;
+			for (int i = 0; i < s.length(); i++) {
+				tempChar = s.charAt(i);
+				switch (tempChar) {
+					case '(':
+						storedChar.push('(');
+						break;
+					case ')':
+						if (!storedChar.isEmpty()) {
+							storedChar.pop();
+						} else {
+							return false;
+						}
+						break;
+				}
+			}
+			if (storedChar.empty()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+}
